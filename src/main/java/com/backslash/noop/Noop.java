c3e077b0aefeb2a0a84dac73507b6d2588401620
Moshe Cohen <moshec@gmail.com>
2023-10-07 20:24:53 -0400

    javadoc

--- src/main/java/com/backslash/noop/Noop.java
+++ src/main/java/com/backslash/noop/Noop.java
@@ -1,6 +1,14 @@
 package com.backslash.noop;
 
+/**
+ * A calls that contains a static noop method.
+ */
 public class Noop {
+    /**
+     * Just a noop. Prints a message to verify the code has been called and returns
+     * a dummy string.
+     * @return      a dummy string
+     */
     static public String noop() {
 	System.out.println("Hi, in noop ");
 	return "string returned from noop";
